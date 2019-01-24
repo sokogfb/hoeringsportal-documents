1a38c1326e7e541155980aba2bf108fbabf911d5
Mikkel Ricky <rimi@aarhus.dk>
2019-01-24 15:45:00 +0100

    HRIN-247: Added eDoc links to list

---
+++ src/Twig/EdocExtension.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Twig;
+
+use App\Entity\EDoc\CaseFile;
+use App\Entity\EDoc\Document;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class EdocExtension extends AbstractExtension
+{
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('edoc_casefile_url', [$this, 'getCaseFileUrl']),
+            new TwigFunction('edoc_casefile_title', [$this, 'getCaseFileTitle']),
+            new TwigFunction('edoc_document_url', [$this, 'getDocumentUrl']),
+            new TwigFunction('edoc_document_title', [$this, 'getDocumentTitle']),
+        ];
+    }
+
+    public function getCaseFileUrl(CaseFile $caseFile)
+    {
+        return $caseFile->getArchiver()->getEdocCaseFileUrl($caseFile);
+    }
+
+    public function getCaseFileTitle(CaseFile $caseFile)
+    {
+        return $caseFile->getData()['edoc']['TitleText'] ?? $caseFile->getCaseFileIdentifier();
+    }
+
+    public function getDocumentUrl(Document $document)
+    {
+        return $document->getArchiver()->getEdocDocumentUrl($document);
+    }
+
+    public function getDocumentTitle(Document $document)
+    {
+        return $document->getData()['edoc']['TitleText'] ?? $document->getDocumentIdentifier();
+    }
+}
