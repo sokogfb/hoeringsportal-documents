3dadb261d2943a92098e772f741bb0647d62b40d
Mikkel Ricky <rimi@aarhus.dk>
2019-01-25 14:21:19 +0100

    HRIN-247: Added built assets

---
+++ public/build/easy_admin.js
@@ -0,0 +1,26 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["easy_admin"],{
+
+/***/ "./assets/css/easy_admin.scss":
+/*!************************************!*\
+  !*** ./assets/css/easy_admin.scss ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "./assets/js/easy_admin.js":
+/*!*********************************!*\
+  !*** ./assets/js/easy_admin.js ***!
+  \*********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(/*! ../css/easy_admin.scss */ "./assets/css/easy_admin.scss");
+
+/***/ })
+
+},[["./assets/js/easy_admin.js","runtime"]]]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9hc3NldHMvY3NzL2Vhc3lfYWRtaW4uc2Nzcz82ODM4Iiwid2VicGFjazovLy8uL2Fzc2V0cy9qcy9lYXN5X2FkbWluLmpzIl0sIm5hbWVzIjpbInJlcXVpcmUiXSwibWFwcGluZ3MiOiI7Ozs7Ozs7OztBQUFBLHVDOzs7Ozs7Ozs7OztBQ0FBQSxtQkFBTyxDQUFDLDREQUFELENBQVAsQyIsImZpbGUiOiJlYXN5X2FkbWluLmpzIiwic291cmNlc0NvbnRlbnQiOlsiLy8gZXh0cmFjdGVkIGJ5IG1pbmktY3NzLWV4dHJhY3QtcGx1Z2luIiwicmVxdWlyZSgnLi4vY3NzL2Vhc3lfYWRtaW4uc2NzcycpO1xuIl0sInNvdXJjZVJvb3QiOiIifQ==
