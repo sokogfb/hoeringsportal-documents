1d3189e11988a5c8c7c0685ff7bb282e41b946a2
Mikkel Ricky <rimi@aarhus.dk>
2019-01-30 20:47:37 +0100

    HRIN-247: Renamed template

--- src/Controller/EDoc/ItemListController.php
+++ src/Controller/EDoc/ItemListController.php
@@ -66,7 +66,7 @@ class ItemListController extends EasyAdminController
             $items = $edoc->getItemList($type);
         }
 
-        return $this->render('admin/edoc/index.html.twig', [
+        return $this->render('admin/edoc/item-list.html.twig', [
             'form' => $form->createView(),
             'archiver' => $archiver,
             'type' => $type,
